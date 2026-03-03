44674c5669b3014fa8ee02fc09432d7d200bb350
AMRUTH SWAMY C P <amruthswamy11@gmail.com>
2026-03-03 21:14:21 +0530

    new deocker file added

--- Air_hand_motion_detecter .py
+++ Air_hand_motion_detecter .py
@@ -8,7 +8,7 @@ def setValues(x):
 
 
 # Creating the trackbars needed for adjusting the marker colour
-cv2.namedWindow("Color detectors")
+cv2.namedWindow("Color detepctors")
 cv2.createTrackbar("Upper Hue", "Color detectors", 153, 180,setValues)
 cv2.createTrackbar("Upper Saturation", "Color detectors", 255, 255,setValues)
 cv2.createTrackbar("Upper Value", "Color detectors", 255, 255,setValues)
